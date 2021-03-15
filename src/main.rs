dea2c535823e4c3a8e33e47890ae2cf15e4433a3
hadi tarhini <hi@how.hadi.is>
2021-03-15 01:53:15 -0700

    Iterate over file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+use std::env;
+use std::io::{self, prelude::*};
+
+fn main() -> io::Result<()> {
+    let args: Vec<_> = env::args().collect();
+    let filename = &args[1];
+    let handle = fs::File::open(filename)?;
+    let reader = io::BufReader::new(handle);
+    for line in collect_lines(reader)?.iter_mut() {
+        split(line);
+    }
+    Ok(())
+}
+
+fn collect_lines<B: BufRead>(reader: B) -> io::Result<Vec<Vec<String>>> {
+    let lines = reader.lines()
+      .collect::<Result<Vec<_>, _>>()?;
+
+    Ok(lines.into_iter()
+      .map(|s| vec![s])
+      .collect())
+}
+
+fn split(line: &mut Vec<String>) {
+    
 }
