b5c68fbbf67ca86d363222f35a242e1a7a0a1890
hadi tarhini <hi@how.hadi.is>
2021-03-15 02:06:23 -0700

    Add line types

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,40 @@ use std::fs;
 use std::env;
 use std::io::{self, prelude::*};
 
+enum Size {
+    Short,
+    Byte,
+    Word,
+    Long
+}
+
+struct LineOfCode {
+    label: Option<String>,
+    initial_ws: u16,
+    instruction: String,
+    size: Size,
+    medial_ws: u16,
+    args: Vec<String>,
+    final_ws: Option<u16>,
+    comment: Option<String>,
+}
+
+struct Comment {
+    ws: u16,
+    text: String
+}
+
+struct Label {
+    name: String
+}
+
 fn main() -> io::Result<()> {
     let args: Vec<_> = env::args().collect();
     let filename = &args[1];
     let handle = fs::File::open(filename)?;
     let reader = io::BufReader::new(handle);
     for line in collect_lines(reader)?.iter_mut() {
-        split(line);
+        parse(line);
     }
     Ok(())
 }
@@ -22,6 +49,6 @@ fn collect_lines<B: BufRead>(reader: B) -> io::Result<Vec<Vec<String>>> {
       .collect())
 }
 
-fn split(line: &mut Vec<String>) {
+fn parse(line: &mut Vec<String>) -> Vec<Line> {
     
 }
